1421b10bb7159d2a9cb0682600757d02fd02ebb2
jserovs <jevgenijs.serovs@gmail.com>
2019-06-13 20:57:04 +0300

    part 3.1 3.2 3.3 3.4

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+    let time = new Date().toString();
+    Date().to
+    console.log(time)
+    res.send(
+        'There is ' + persons.length + ' people</br>'
+        + time)
+
+
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        console.log(person)
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        persons.splice(id,1)
+        console.log(persons)
+        res.status(204).end()
+    } else {
+        res.status(404).end()
+    }
+    
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
