788bcb3e8738a99bbec5b83434f7f16ee51645ba
jserovs <jevgenijs.serovs@gmail.com>
2019-06-29 16:49:35 +0300

    part 3.9, 3.10, 3.11

---
+++ build/precache-manifest.aa46db06d76b47de1821a293065200ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab05f20235870676c28fccb4598b596e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "481322eed0722f95aaec",
+    "url": "/static/css/main.d1e5abf4.chunk.css"
+  },
+  {
+    "revision": "d34d7c3e7e8add90f63c",
+    "url": "/static/js/2.56f586ee.chunk.js"
+  },
+  {
+    "revision": "481322eed0722f95aaec",
+    "url": "/static/js/main.d090ead0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const cors = require('cors')
 app.use(bodyParser.json())
 app.use(morgan('combined'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
